32ee187d5f08eb8e24d973fcd239e8c0a994585e
Tejas <2020.tejas.khedkar@ves.ac.in>
2022-02-28 22:33:43 +0530

    removed unwanted comments

--- db.js
+++ db.js
@@ -1,8 +1,6 @@
 const mongoose = require("mongoose");
-// const mongoUri =
-//   "mongodb+srv://Tmk5250:Tmk%409869865250@cluster0.bgmob.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-const mongoUri =
-  "mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&ssl=false";
+const mongoUri =process.env.MONGO_URI_CLOUD_BASED
+// const mongoUri ="mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&ssl=false"; 
 
 
 const connection = () => {
